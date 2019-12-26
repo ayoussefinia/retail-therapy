226b9c4c75b43bfc757f072af8741f76e4676d25
ayoussefinia <alex.youssefinia@gmail.com>
2019-12-26 15:14:20 -0600

    added user model and api endpoints for signup and login using passport-jwt strategy

--- models/user.js
+++ models/user.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-  username: {type: String, required: false, unique: true },
-  password: {type: String, required: false}
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = User = mongoose.model("users", UserSchema);
--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+
+
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 // Add routes, both API and view
 app.use(routes);
 
